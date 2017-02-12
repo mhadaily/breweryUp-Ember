881f77631e700ed33af1d45bfc007dda75895341
Majid <mhadaily@gmail.com>
2017-02-12 14:55:26 +0800

    style model

---
+++ app/models/style.js
@@ -0,0 +1,18 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  category: DS.attr(),
+  srmMax: DS.attr(),
+  ibuMax: DS.attr(),
+  srmMin: DS.attr(),
+  description: DS.attr(),
+  fgMin: DS.attr(),
+  ibuMin: DS.attr(),
+  createDate: DS.attr(),
+  fgMax: DS.attr(),
+  abvMax: DS.attr(),
+  ogMin: DS.attr(),
+  abvMin: DS.attr(),
+  name: DS.attr(),
+  categoryId: DS.attr()
+});
