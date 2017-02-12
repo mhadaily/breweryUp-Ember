b94e77ca260338f56a5968ab9bcfb302a2a76915
Majid <mhadaily@gmail.com>
2017-02-12 16:17:03 +0800

    yeasts model

---
+++ app/models/yeast.js
@@ -0,0 +1,20 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr(),
+  description: DS.attr(),
+  yeastType: DS.attr(),
+  attenuationMin: DS.attr(),
+  attenuationMax: DS.attr(),
+  fermentTempMin: DS.attr(),
+  fermentTempMax: DS.attr(),
+  alcoholToleranceMin: DS.attr(),
+  alcoholToleranceMax: DS.attr(),
+  productId: DS.attr(),
+  supplier: DS.attr(),
+  yeastFormat: DS.attr(),
+  category: DS.attr(),
+  categoryDisplay: DS.attr(),
+  createDate: DS.attr(),
+  updateDate: DS.attr()
+});
