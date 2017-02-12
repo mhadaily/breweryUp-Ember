031aa3809e7319098ec43173940b43434223436f
Majid <mhadaily@gmail.com>
2017-02-12 14:35:06 +0800

    remove Ajax method, add comments

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,14 +1,21 @@
-import Ember from 'ember';
 import DS from 'ember-data';
 import config from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend({
   host: config.apiUrl,
   namespace: 'v2',
-  ajax: function(url, type, hash) {
-    if (Ember.isEmpty(hash)) hash = {};
-    if (Ember.isEmpty(hash.data)) hash.data = {};
-    hash.data.key = config.apiKey;
-    return this._super(url, type, hash);
-  }
+
+  /*
+    Ajax will override ajax call and build a new one. In case, we want to send always a Param to URL so
+     we can have this method to help us to send on every single ajax call in Ember.
+     Since, I have written a Lambda Function and BreweryDB doesn't support browser call which make sense because
+     of revealing KEY to public, I have disabled this, However, I will keep this as a reference
+   */
+
+  //ajax(url, type, hash) {
+  //  if (Ember.isEmpty(hash)) hash = {};
+  //  if (Ember.isEmpty(hash.data)) hash.data = {};
+  //  hash.data.key = config.apiKey;
+  //  return this._super(url, type, hash);
+  //}
 });
