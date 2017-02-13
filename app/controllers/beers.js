7124c194236a2e8609774eba68953d60666b62d4
Majid <mhadaily@gmail.com>
2017-02-14 01:00:20 +0800

    a bit refinement on UI

--- app/controllers/beers.js
+++ app/controllers/beers.js
@@ -4,10 +4,12 @@ export default Ember.Controller.extend({
   queryParams: [
     'page',
   ],
+  isLoading: false,
   page: 1,
   total_pages: 60877,
   actions: {
     prev(){
+      this.set('isLoading', true);
       let page = this.get('page');
       let newPage = parseInt(page - 1);
       if (newPage < 1) {
@@ -15,14 +17,25 @@ export default Ember.Controller.extend({
         return;
       }
       this.set('page', newPage);
+      setTimeout(() => {
+        this.set('isLoading', false);
+      }, 2000);
     },
     next(){
+      this.set('isLoading', true);
       let page = this.get('page');
       let newPage = parseInt(page + 1);
       this.set('page', newPage);
+      setTimeout(() => {
+        this.set('isLoading', false);
+      }, 2000);
     },
     updateValue(value){
+      this.set('isLoading', true);
       this.set('page', value);
+      setTimeout(() => {
+        this.set('isLoading', false);
+      }, 2000);
     }
   }
 });
